1c64e101d57d245c636cd192005ea85cdfc42954
Tushar <tushargour004@gmail.com>
2024-05-26 13:32:12 +0530

    created game

--- lib/functions/king_ckeck.dart
+++ lib/functions/king_ckeck.dart
@@ -1,6 +1,14 @@
 import 'package:chess/components/piece.dart';
 import 'package:chess/functions/helpers.dart';
 
+bool isAlreadyInAttacker(List<List<int>> attackers, List<int> toCheck) {
+  for (var pair in attackers) {
+    if (pair[0] == toCheck[0] && pair[1] == toCheck[1]) return true;
+  }
+
+  return false;
+}
+
 List<List<int>> isKingInCheck(
   List<List<ChessPiece?>> board,
   int row,
@@ -22,15 +30,137 @@ List<List<int>> isKingInCheck(
   // PAWN ( Diagonally )
   if (isInBoard(row + direction, col - 1) &&
       board[row + direction][col - 1] != null &&
-      board[row + direction][col - 1]!.isWhite != kingPiece.isWhite) {
+      board[row + direction][col - 1]!.isWhite != kingPiece.isWhite &&
+      board[row + direction][col - 1]!.type == ChessPieceType.pawn) {
     attacking_pieces.add([row + direction, col - 1]);
   }
 
   if (isInBoard(row + direction, col + 1) &&
       board[row + direction][col + 1] != null &&
-      board[row + direction][col + 1]!.isWhite != kingPiece.isWhite) {
+      board[row + direction][col + 1]!.isWhite != kingPiece.isWhite &&
+      board[row + direction][col + 1]!.type == ChessPieceType.pawn) {
     attacking_pieces.add([row + direction, col + 1]);
   }
+
+  // ROOK ( up down left right )
+  final rookMoves = [
+    [-1, 0], // up
+    [1, 0], // down
+    [0, -1], // left
+    [0, 1] // right
+  ];
+
+  for (var move in rookMoves) {
+    int i = 1;
+    while (true) {
+      int newRow = row + i * move[0];
+      int newCol = col + i * move[1];
+
+      if (!isInBoard(newRow, newCol)) break;
+
+      if (board[newRow][newCol] != null) {
+        if (!isAlreadyInAttacker(attacking_pieces, [newRow, newCol]) &&
+            board[newRow][newCol]!.isWhite != kingPiece.isWhite &&
+            board[newRow][newCol]!.type == ChessPieceType.rook) {
+          attacking_pieces.add([newRow, newCol]); // can kill
+        }
+        break; // blocked
+      }
+
+      i++;
+    }
+  }
+
+  // KNIGHT ( 2 and 1 )
+  final knightMoves = [
+    [-2, -1],
+    [-2, 1],
+    [-1, -2],
+    [-1, 2],
+    [1, -2],
+    [1, 2],
+    [2, -1],
+    [2, 1],
+  ];
+
+  for (var move in knightMoves) {
+    int newRow = row + move[0] * direction;
+    int newCol = col + move[1] * direction;
+
+    if (!isInBoard(newRow, newCol)) continue;
+
+    if (board[newRow][newCol] != null) {
+      if (!isAlreadyInAttacker(attacking_pieces, [newRow, newCol]) &&
+          board[newRow][newCol]!.isWhite != kingPiece.isWhite &&
+          board[newRow][newCol]!.type == ChessPieceType.knight) {
+        attacking_pieces.add([newRow, newCol]); // can kill
+      }
+      continue; // blocked
+    }
+  }
+
+  // BISHOPS ( top_left top_right bottom_left bottom_right )
+  final bishopMoves = [
+    [-1, -1], // up left
+    [-1, 1], // up right
+    [1, -1], // down left
+    [1, 1], // down right
+  ];
+
+  for (var move in bishopMoves) {
+    int i = 1;
+    while (true) {
+      int newRow = row + i * move[0];
+      int newCol = col + i * move[1];
+
+      if (!isInBoard(newRow, newCol)) break;
+
+      if (board[newRow][newCol] != null) {
+        if (!isAlreadyInAttacker(attacking_pieces, [newRow, newCol]) &&
+            board[newRow][newCol]!.isWhite != kingPiece.isWhite &&
+            board[newRow][newCol]!.type == ChessPieceType.bishop) {
+          attacking_pieces.add([newRow, newCol]); // can kill
+        }
+        break; // blocked
+      }
+
+      i++;
+    }
+  }
+
+  // QUEEN ( bishop + rook )
+  final queenMoves = [
+    [-1, 0], // up
+    [1, 0], // down
+    [0, -1], // left
+    [0, 1], // right
+    [-1, -1], // up eft
+    [-1, 1], // up right
+    [1, -1], // down left
+    [1, 1], // down right
+  ];
+
+  for (var move in queenMoves) {
+    int i = 1;
+    while (true) {
+      int newRow = row + i * move[0];
+      int newCol = col + i * move[1];
+
+      if (!isInBoard(newRow, newCol)) break;
+
+      if (board[newRow][newCol] != null) {
+        if (!isAlreadyInAttacker(attacking_pieces, [newRow, newCol]) &&
+            board[newRow][newCol]!.isWhite != kingPiece.isWhite &&
+            board[newRow][newCol]!.type == ChessPieceType.queen) {
+          attacking_pieces.add([newRow, newCol]); // can kill
+        }
+        break; // blocked
+      }
+
+      i++;
+    }
+  }
+  print(attacking_pieces);
 
   return attacking_pieces;
 }
